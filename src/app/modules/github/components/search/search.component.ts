1b11a30729ceb3cda8841bf5b8af9117ccd2e7d2
Jonathan Lopez <jonathan.lopez@lifeomic.com>
2020-11-27 17:39:48 -0600

    Adds debounce

--- src/app/modules/github/components/search/search.component.ts
+++ src/app/modules/github/components/search/search.component.ts
@@ -5,6 +5,12 @@ import {
 } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 
+import { debounceTime } from 'rxjs/internal/operators/debounceTime';
+import {
+  distinctUntilChanged
+} from 'rxjs/internal/operators/distinctUntilChanged';
+import { Subject } from 'rxjs/internal/Subject';
+
 import { User } from '../../interfaces/user.interface';
 import { GithubService } from '../../services/github.service';
 
@@ -16,14 +22,22 @@ import { GithubService } from '../../services/github.service';
 export class SearchComponent implements AfterViewInit {
   users: User[];
   total = 0;
-  search = 'thisdot';
+  searchParam = 'thisdot';
+  searchParamChanged: Subject<string> = new Subject<string>();
   page = 1;
   @ViewChild(MatPaginator) paginator: MatPaginator;
-  constructor(private githubService: GithubService) {}
+  
+  constructor(private githubService: GithubService) {
+    this.searchParamChanged
+      .pipe(debounceTime(500), distinctUntilChanged())
+      .subscribe((model) => {
+        this.searchParam = model;
+        this.getUsers();
+      });
+  }
 
   ngAfterViewInit(): void {
     this.paginator.page.subscribe((data) => {
-      console.log(data);
       this.page = data.pageIndex + 1;
       this.getUsers();
     });
@@ -31,9 +45,15 @@ export class SearchComponent implements AfterViewInit {
   }
 
   getUsers(): void {
-    this.githubService.search(this.search, this.page).subscribe((response) => {
-      this.users = response.items;
-      this.total = response.totalCount;
-    });
+    this.githubService
+      .search(this.searchParam, this.page)
+      .subscribe((response) => {
+        this.users = response.items;
+        this.total = response.totalCount;
+      });
+  }
+
+  onSearchParam(value: string): void {
+    this.searchParamChanged.next(value);
   }
 }
