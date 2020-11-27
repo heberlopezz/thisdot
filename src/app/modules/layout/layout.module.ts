a568da88a472fb31db7cc18ec8ba27d562ab930e
Jonathan Lopez <jonathan.lopez@lifeomic.com>
2020-11-27 16:18:38 -0600

    Creates layout

--- src/app/modules/layout/layout.module.ts
+++ src/app/modules/layout/layout.module.ts
@@ -1,14 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { HeaderComponent } from './components/header/header.component';
 import { LayoutComponent } from './components/layout/layout.component';
 import { LayoutRoutingModule } from './layout-routing.module';
 
 const components = [HeaderComponent, LayoutComponent];
+const materialModules = [MatToolbarModule];
 
 @NgModule({
   declarations: [components],
-  imports: [CommonModule, LayoutRoutingModule],
+  imports: [CommonModule, LayoutRoutingModule, materialModules],
 })
 export class LayoutModule {}
